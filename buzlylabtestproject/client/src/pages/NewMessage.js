419352bbdd17670f5f49ab39190a3ba576a55329
dev-giant <artyom080396@outlook.com>
2022-03-25 02:06:01 +0200

    03250205commit

--- buzlylabtestproject/client/src/pages/NewMessage.js
+++ buzlylabtestproject/client/src/pages/NewMessage.js
@@ -3,14 +3,21 @@ import React from "react";
 import { MainBox } from "./Home";
 
 export const NewMessage = () => {
+
+    const handleSubmit = () => {
+        
+    }
+
     return(
         <MainBox>
             <Box>
-                <TextareaAutosize
-                    placeholder="place new message here"
-                    minRows={5}>
-                </TextareaAutosize>
-                <Button variant="contained">Submit</Button>
+                <form  onSubmit={handleSubmit}>
+                    <TextareaAutosize
+                        placeholder="place new message here"
+                        minRows={5}>
+                    </TextareaAutosize>
+                    <Button variant="contained">Submit</Button>
+                </form>
             </Box>
         </MainBox>
     );
