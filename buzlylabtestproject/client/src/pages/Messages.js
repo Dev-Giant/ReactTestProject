2a63526140c02c94b46c637e005720ca16d88530
dev-giant <artyom080396@outlook.com>
2022-03-25 09:43:18 +0200

    03250943commit

--- buzlylabtestproject/client/src/pages/Messages.js
+++ buzlylabtestproject/client/src/pages/Messages.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainBox } from './Home'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { fetchMessages } from '../redux/actions/messages';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Button } from '@mui/material';
 
 export const Messages = () => {
     const listMessages = useSelector(state => state.messages)
-    const { messages, loading, error } = listMessages;
+    const { messages } = listMessages;
+    const [ displayCount, setDisplayCount ] = useState(1);
 
     const dispatch = useDispatch();
     useEffect(() => {
@@ -17,11 +18,14 @@ export const Messages = () => {
     return(
         <MainBox>
             <Box className="mainDiv">
-                {messages.map((item, index) =>
-                    <Link to={`/message/${item?._id}`}>
-                        <Typography variant ="body1" key={index}>{item?.description}</Typography>
+                {messages.filter((message, index) => {
+                        return index < 5 * displayCount
+                }).map((item, index) =>
+                    <Link to={`/message/${item?._id}`} key={index}>
+                        <Typography variant ="body1">{item?.description}</Typography>
                     </Link>
                 )}
+                <Button onClick = {() => setDisplayCount(displayCount + 1)}>Show More</Button>
             </Box>
         </MainBox>
     )
